df52c180a16462ba9710c371511dc3bb9a6888c6
Gediminas Norkus <gediminasno91@gmail.com>
2024-01-04 20:15:15 +0200

    "Add files"

---
+++ PD2/src/lt/techin/Main_pd_2_1.java
@@ -0,0 +1,9 @@
+package lt.techin;
+
+public class Main_pd_2_1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Learning Java now ");
+        System.out.println("Programming is fun");
+    }
+}

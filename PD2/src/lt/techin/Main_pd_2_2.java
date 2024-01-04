9481b785fe2c31f6c69f195e874863cb20bf2c84
Gediminas Norkus <gediminasno91@gmail.com>
2024-01-04 20:30:55 +0200

    "Add Main_pd_2_2"

---
+++ PD2/src/lt/techin/Main_pd_2_2.java
@@ -0,0 +1,15 @@
+package lt.techin;
+
+public class Main_pd_2_2 {
+    public static void main(String[] args) {
+
+
+        System.out.println("J      A      V     V     A ");
+        System.out.println("J     A A      V   V     A A");
+        System.out.println("J    A A A      V V     A A A");
+        System.out.println("JJ  A     A      V     A     A");
+    }
+}
+
+
+

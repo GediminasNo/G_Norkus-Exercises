2f81754cc40c60ea0c67b849e0408beb3ae54579
Gediminas Norkus <gediminasno91@gmail.com>
2024-01-05 20:31:38 +0200

    "Main_pd_2_5 task done"

---
+++ PD2/src/lt/techin/Main_pd_2_5.java
@@ -0,0 +1,12 @@
+package lt.techin;
+
+public class Main_pd_2_5 {
+    public static void main(String[] args) {
+        System.out.println("Komentaras vienos eilutes \t// Komentaras ");
+        // Komentaras vienos eilutes
+
+
+        System.out.println("Komentaras multi eilutems \t/* Komentaras */");
+        /*  Komentaras multi eilutems */
+    }
+}

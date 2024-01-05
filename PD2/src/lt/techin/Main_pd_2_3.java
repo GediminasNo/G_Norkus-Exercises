016a277ece9d54f31ed7c482b9551f7c15712a70
Gediminas Norkus <gediminasno91@gmail.com>
2024-01-05 20:15:33 +0200

    "Main_pd_2_3 task done"

---
+++ PD2/src/lt/techin/Main_pd_2_3.java
@@ -0,0 +1,12 @@
+package lt.techin;
+
+public class Main_pd_2_3 {
+    public static void main(String[] args) {
+        System.out.println("a\t\ta^2\t\ta^3\t\ta^4");
+        System.out.println("---------------------------");
+        System.out.println("1\t\t1\t\t1\t\t1");
+        System.out.println("2\t\t4\t\t8\t\t16");
+        System.out.println("3\t\t9\t\t27\t\t81");
+        System.out.println("4\t\t16\t\t64\t\t256");
+    }
+}

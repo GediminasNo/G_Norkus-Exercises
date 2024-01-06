5b9ac9578e5b847d0aacec130120c0288068616c
Gediminas Norkus <gediminasno91@gmail.com>
2024-01-06 21:52:08 +0200

    "Main_pd_3_1 done"

---
+++ PD3/src/lt/techin/Main_pd_3_1.java
@@ -0,0 +1,24 @@
+package lt.techin;
+
+public class Main_pd_3_1 {
+    public static void main(String[] args) {
+        byte SkaiciusByte = 100;
+        short SkaiciuShort = 1000;
+        int SkaiciusInt =  10000;
+        long SkaiciusLong = 1000000000L;
+        double SkaiciusDouble = 3.15;
+        float SkaiciusFloat = 2.15f;
+        boolean orItIsTrue = true;
+        char simbolisChar = 'A';
+        System.out.println(SkaiciusByte);
+        System.out.println(SkaiciuShort);
+        System.out.println(SkaiciusInt);
+        System.out.println(SkaiciusLong);
+        System.out.println(SkaiciusDouble);
+        System.out.println(SkaiciusFloat);
+        System.out.println(orItIsTrue);
+        System.out.println(simbolisChar);
+
+
+    }
+}

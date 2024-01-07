3bfdc2c3c552d1de93baf9ea83324969fc73e2be
Gediminas Norkus <gediminasno91@gmail.com>
2024-01-07 14:08:39 +0200

    "Main_pd_3_8 done"

---
+++ PD3/src/lt/techin/Main_pd_3_8.java
@@ -0,0 +1,11 @@
+package lt.techin;
+
+public class Main_pd_3_8 {
+    public static void main(String[] args) {
+        double pirmaEilute = (7.5*6.5)-(4.5*3)/(47.5-5.5);
+        double antraEulute = 12.78/(15.4-2.75);
+
+        System.out.println(pirmaEilute + "\t pirma eilute");
+        System.out.println(antraEulute + "\t antra eilute");
+    }
+}

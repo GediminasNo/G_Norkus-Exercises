41338d686822383264de3b27863e330aa6498d11
Gediminas Norkus <gediminasno91@gmail.com>
2024-01-07 21:08:12 +0200

    "Main_pd_4_4 done"

---
+++ PD4/src/Main_pd_4_4.java
@@ -0,0 +1,23 @@
+public class Main_pd_4_4 {
+    public static void main(String[] args) {
+        int pirmas_skaicius = 10;
+        int antrasSkaicius = 20;
+
+        pirmas_skaicius+=5;
+        System.out.println(pirmas_skaicius);
+
+        pirmas_skaicius-=12;
+        System.out.println(pirmas_skaicius);
+
+        pirmas_skaicius*=80;
+        System.out.println(pirmas_skaicius);
+
+        pirmas_skaicius/=12;
+        System.out.println(pirmas_skaicius);
+
+        pirmas_skaicius%=3;
+        System.out.println(pirmas_skaicius);
+
+
+    }
+}

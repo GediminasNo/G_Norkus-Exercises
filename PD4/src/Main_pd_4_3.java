e429d4a72001b366e8affb306d43cea7f7ec630a
Gediminas Norkus <gediminasno91@gmail.com>
2024-01-07 21:02:48 +0200

    "Main_pd_4_3 done"

---
+++ PD4/src/Main_pd_4_3.java
@@ -0,0 +1,13 @@
+public class Main_pd_4_3 {
+    public static void main(String[] args) {
+        boolean pirmasTrue = true;
+        boolean antrasFalse = false;
+
+        boolean resultAnd = pirmasTrue && antrasFalse;
+        System.out.println("Rezultatas and " + resultAnd);
+
+        boolean resultOr = pirmasTrue || antrasFalse;
+        System.out.println("Rezultatas or " + resultOr);
+
+    }
+}

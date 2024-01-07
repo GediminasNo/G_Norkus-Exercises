3a2e7ea0fbdcfcfefcf3eca1d7d0b0da8c6b2008
Gediminas Norkus <gediminasno91@gmail.com>
2024-01-07 13:54:29 +0200

    "Main_pd_3_6 done"

---
+++ PD3/src/lt/techin/Main_pd_3_6.java
@@ -0,0 +1,13 @@
+package lt.techin;
+
+public class Main_pd_3_6 {
+    public static void main(String[] args) {
+        int skaiciusInt = 10;
+        Integer intWraper = Integer.valueOf(skaiciusInt);
+        System.out.println(intWraper);
+
+        double skaiciusDouble = 3.15;
+        Double doubleWraper = Double.valueOf(skaiciusDouble);
+        System.out.println(doubleWraper);
+    }
+}

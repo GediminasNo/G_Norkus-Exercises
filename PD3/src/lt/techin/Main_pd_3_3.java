2b815e3dd5143d93156188e52acdca607341b18b
Gediminas Norkus <gediminasno91@gmail.com>
2024-01-07 13:14:43 +0200

    "Main_pd_3_3 done"

---
+++ PD3/src/lt/techin/Main_pd_3_3.java
@@ -0,0 +1,18 @@
+package lt.techin;
+
+public class Main_pd_3_3 {
+    public static void main(String[] args) {
+        float floatPirmas = 1.1f;
+        float floatAntras = 2.2f;
+
+        double doublePirmas = 1.1;
+        double doubleAntras = 2.2;
+
+        float sumaFloat = floatPirmas+floatAntras;
+        double sumaDouble = doublePirmas+doubleAntras;
+
+        System.out.println(sumaFloat);
+        System.out.println(sumaDouble);
+
+    }
+}

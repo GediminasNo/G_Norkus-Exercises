a693264a58c9684031649d7b5104b341eda1683a
Gediminas Norkus <gediminasno91@gmail.com>
2024-01-08 20:07:19 +0200

    "Main_pd_5_2 done "

---
+++ PD5/src/Main_pd_5_2.java
@@ -0,0 +1,12 @@
+public class Main_pd_5_2 {
+    public static void main(String[] args) {
+        int dydis = 10;
+
+        for(int i = 1;i<=dydis;i++){
+            for(int j =1 ; j<=i;j++){
+                System.out.println(i*j);
+            }
+            System.out.println();
+        }
+    }
+}
